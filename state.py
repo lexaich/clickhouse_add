9c6ffd5eccc1fcda76c63440a2f4183594e15792
lexaich <lexalord95@mail.ru>
2018-10-16 21:54:30 +0300

    added coins

--- state.py
+++ state.py
@@ -1,14 +1,12 @@
+# -*- coding: utf-8 -*-
 from clickhouse_driver import Client
 import requests
 
 client = Client('localhost')
-# 200000 записей
-url_str = "http://min-api.cryptocompare.com/data/histoday?fsym=SMT*&tsym=BTC&limit="
-nums = "200000"
 
-url = '{0}{1}'.format(url_str, nums)
-data = requests.get(url).json()["Data"]
-
+url_coins = "https://www.cryptocompare.com/api/data/coinlist/"
+url_str_frst = "https://min-api.cryptocompare.com/data/histoday?fsym="
+url_str_scnd = "*&tsym=BTC&limit=1000"
 schema = {
 	"open": "Float32",
 	"volumeto": "Float32",
@@ -18,9 +16,21 @@ schema = {
 	"low": "Float32",
 	"close": "Float32"
 }
-
 schema_create = ", ".join(["{} {}".format(a, b) for a, b in schema.items()])
-# client.execute('DROP TABLE IF EXISTS blocks')
-client.execute('CREATE TABLE IF NOT EXISTS blocks ({}) ENGINE = MergeTree() ORDER BY (time)'.format(schema_create))
 schema_insert = ",".join(schema.keys())
-client.execute('INSERT INTO blocks ({}) VALUES'.format(schema_insert), data)
+coins = requests.get(url_coins).json()["Data"]
+
+for coin in coins:
+	url = '{0}{1}{2}'.format(url_str_frst,coin,url_str_scnd)
+	req = requests.get(url)
+	if(req.status_code==200):
+		data = req.json()["Data"]
+		if data:
+			print(coin)
+			try:
+				client.execute('DROP TABLE IF EXISTS {0}_blocks'.format(coin))
+				client.execute('CREATE TABLE IF NOT EXISTS {0}_blocks ({1}) ENGINE = MergeTree() ORDER BY (time)'.format(coin,schema_create))
+				client.execute('INSERT INTO {0}_blocks ({1}) VALUES'.format(coin,schema_insert), data)
+			except:
+				print('exept symbol')
+			
